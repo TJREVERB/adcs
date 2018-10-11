5500ad1e3bfc5774740185440b60f77bd0a3d1b6
unknown <bharathdileepkumar@gmail.com>
2018-10-11 18:51:08 -0400

    in progress - will finish soon

--- getthetaerr.py
+++ getthetaerr.py
@@ -1,5 +1,6 @@
 # getthetaerr
 # Calculates error theta for each axis based on error quaternion
+# Translated by Bharath Dileepkumar 10/11/2018
 import numpy as np
-def getthetaerr([2;3;4]):
-    q = np.transpose()
+def getthetaerr(q):
+    q = np.transpose(q)
