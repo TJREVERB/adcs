8bf1a4384d9c9e93963062fddaa2dc90bc3ec29b
Jason Chen <jay.chen135@gmail.com>
2018-10-11 17:37:30 -0400

    work

--- getDCM.py
+++ getDCM.py
@@ -1,2 +1,9 @@
 # PRIORITY
+"""
 bV and sV converted on board
+Know instantaneous time, at any measurement (possibly from the GPS)
+Convert GPS time to UTC Time
+Interface between data and ADCS
+ecef2eci (should be from a toolbox)
+
+"""
