78c01d3b7ffb721c44f72af6efd9ad46ad6038ab
ayusher <42046470+ayusher@users.noreply.github.com>
2018-11-09 15:09:42 -0500

    Update wfun.py

--- wfun.py
+++ wfun.py
@@ -5,12 +5,8 @@
 #  sc = Struct describing CubeSat
 # inertia method needs to be written, use numpy to find derivative in numpy
 def wfun(t,w0, torque, sc):
-dwdt = wfun(t,w0, torque, sc)
-
-
-
-function [dwdt] = wfun(t,w0,torque,sc)
-
-w0 = w0(:)';
-torque = torque(:)';
-dwdt = sc.inertia/(torque'-cross(w0',(sc.inertia*w0'))');
+  dwdt = wfun(t,w0, torque, sc)
+  w0=np.reshape(w0, (1,-1))
+  torque=np.reshape(torque, (1,-1))
+  dwdt = sc.inertia/(torque.getH()-np.cross(w0.getH(),(sc.inertia*w0.getH())).getH())
+  return(dwdt)
