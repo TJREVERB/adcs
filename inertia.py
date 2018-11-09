eb4105791a22633bfa1a75f1d7c2ea285c641911
ayusher <42046470+ayusher@users.noreply.github.com>
2018-11-09 15:14:17 -0500

    Delete inertia.py

--- inertia.py
+++
@@ -1,7 +0,0 @@
-#this code is a function used in wfun.py
-# T = initial torque
-# J = inertia
-# W = angular veolcity
-# t = time
-# use numpy to calculate derivative with respect to time dw/dt
-def inertia(T, J, W, t):
