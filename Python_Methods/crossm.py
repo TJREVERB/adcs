773253c572076c85151796b89fea4f07f80774fd
Jason Chen <jay.chen135@gmail.com>
2018-11-19 15:32:43 -0500

    removed a lot

--- Python_Methods/crossm.py
+++
@@ -1,8 +0,0 @@
-#converted by Bhargava Elavarthi
-import numpy as np
-def crossm(u):
-    np = [0 -u(3) u(2);  u(3) 0 -u(1); -u(2) u(1) 0]
-    return np
-
-matrix = np.array([1,2];[2,3])
-print(crossm(matrix))
--- Python_Methods/qmult.py
+++ Python_Methods/qmult.py
@@ -1,4 +1,3 @@
-#BEING WORKED ON BY AYUSH
 import numpy as np
 def qmult(q1,q2):
     q1 = np.squeeze(np.asarray(q1))
@@ -8,5 +7,3 @@ def qmult(q1,q2):
     comp1 = np.matrix([[q2.item(3), q2.item(2), -q2.item(1), q2.item(0)],[-q2.item(2), q2.item(3), q2.item(0), q2.item(1)],[q2.item(1), -q2.item(0), q2.item(3), q2.item(2)],[-q2.item(0), -q2.item(1), -q2.item(2), q2.item(3)]])
     comp2 = np.matrix([[q1.item(0)],[q1.item(1)],[q1.item(2)],[q1.item(3)]])
     return((comp1*comp2).getH())
-
-
