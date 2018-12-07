c4d4898e1f81414d43101a0479d91f4b914b7cca
ayusher <42046470+ayusher@users.noreply.github.com>
2018-12-07 14:57:44 -0500

    Update dcm2gibbs.py

--- Python_Methods/dcm2gibbs.py
+++ Python_Methods/dcm2gibbs.py
@@ -9,7 +9,7 @@ import math
 
 def dcm2gibbs(dcm):
     g = ((1, 3))
-    g.zeros(3)
+    np.zeros(3)
     g[1, 1] = -1 *math.atan(dcm[2,1]/dcm[2,2])
     g[1, 2] = math.asin(dcm[2, 3])
     g[1, 3] = math.atan(dcm[1,3]/dcm[3,3])
