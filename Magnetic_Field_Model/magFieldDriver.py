3f4056f7788ca7d56574093161eb173aa69593d1
unknown <bharathdileepkumar@gmail.com>
2018-12-20 17:54:43 -0500

    almost done

--- Magnetic_Field_Model/magFieldDriver.py
+++ Magnetic_Field_Model/magFieldDriver.py
@@ -1,13 +1,12 @@
 #Created by Bharath Dileepkumar
-#Last Edited: December 6, 2018
+#Last Edited: December 20, 2018
 
 import decyear, getDCM, sun_vec, utc2jul, wrldmagm, datetime
 import numpy as np
 import pymap3d as pm
 class magFieldDriver:
      def testFunction(self, bV, sV, lat, long, alt, time):
-         #epoch_date = datetime.datetime(1900, 1, 1, 0, 0)
-         jdays = utc2jul.utc2jul(time) #- utc2jul.utc2jul(epoch_date)
+         jdays = utc2jul.utc2jul(time)
          sI = sun_vec.sun_vec(jdays-2444244.5)
          print("sI:")
          print(sI)
@@ -15,14 +14,26 @@ class magFieldDriver:
          print()
          gm = wrldmagm.wrldmagm("WMM2015.COF")
          bI = np.matrix(gm.wrldmagm(lat, long, alt, decyear.decyear(time)))
-         print("bI:")
+         bI = bI/np.linalg.norm(bI)
+         print("bI: (NED)")
          print(bI)
          print("-----------------------------------------")
          print()
          bI = np.squeeze(np.asarray(bI))
          bI = pm.ned2ecef(bI[0], bI[1], bI[2], lat, long, alt)
+         bIECEF = np.matrix(bI).transpose()
+         bIECEF = bI/np.linalg.norm(bI)
+         print("bI: (ECEF)")
+         print(bIECEF)
+         print("-----------------------------------------")
+         print()
          bI = pm.ecef2eci(bI, time)
          bI = np.matrix(bI).transpose()
+         bI = bI/np.linalg.norm(bI)
+         print("bI: (ECI)")
+         print(bI)
+         print("------------------------------")
+         print()
          print("DCM:")
          return getDCM.getDCM(bV, sV, bI, sI)
 
@@ -34,32 +45,6 @@ class magFieldDriver:
          lon_deg = -1.348386750055788e+02
          alt_meter = 3.968562753276266e+05
          return self.testFunction(bV, sV, lat_deg, lon_deg, alt_meter, new_date)
-
-    def testFunction(self, bV, sV, lat, long, alt, time):
-        #epoch_date = datetime.datetime(1900, 1, 1, 0, 0)
-        jdays = utc2jul.utc2jul(time) #- utc2jul.utc2jul(epoch_date)
-        sI = sun_vec.sun_vec(jdays)
-        gm = wrldmagm.wrldmagm("WMM.COF")
-        #print(bV)
-        #print(sV)
-        bI = np.matrix(gm.wrldmagm(lat, long, alt, decyear.decyear(time)))
-        bI = np.squeeze(np.asarray(bI))
-        bV = np.matrix(pm.ecef2eci(bI, time)).transpose()
-        print(bV)
-        print(sV)
-        #print(bI)
-        #print(sI)
-        bI = np.matrix(bI).transpose()
-        return getDCM.getDCM(bV, sV, bI, sI)
-        #return -1
-    def mainTester(self):
-        new_date = datetime.datetime(2018, 11, 8, 12, 0, 0)
-        sV = np.matrix([0.736594581345171, -0.321367778737346, 0.595106018724694]).transpose()
-        bV = np.matrix([ 0.593302194154829, -0.297353472232347, -0.748046401610510]).transpose()
-        lat_deg = 5.335745187657780
-        lon_deg = -1.348386750055788e+02
-        alt_meter = 3.968562753276266e+05
-        return self.testFunction(bV, sV, lat_deg, lon_deg, alt_meter, new_date)
 
 driver = magFieldDriver()
 print(driver.mainTester())
