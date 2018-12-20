19effe05cce0c2af686f903f9869be280feac445
Jason <jay.chen135@gmail.com>
2018-12-20 16:41:49 -0500

    corrected some magfield stuff

--- Magnetic_Field_Model/magFieldDriver.py
+++ Magnetic_Field_Model/magFieldDriver.py
@@ -5,7 +5,6 @@ import decyear, getDCM, sun_vec, utc2jul, wrldmagm, datetime
 import numpy as np
 import pymap3d as pm
 class magFieldDriver:
-<<<<<<< HEAD
      def testFunction(self, bV, sV, lat, long, alt, time):
          #epoch_date = datetime.datetime(1900, 1, 1, 0, 0)
          jdays = utc2jul.utc2jul(time) #- utc2jul.utc2jul(epoch_date)
@@ -35,7 +34,7 @@ class magFieldDriver:
          lon_deg = -1.348386750055788e+02
          alt_meter = 3.968562753276266e+05
          return self.testFunction(bV, sV, lat_deg, lon_deg, alt_meter, new_date)
-=======
+
     def testFunction(self, bV, sV, lat, long, alt, time):
         #epoch_date = datetime.datetime(1900, 1, 1, 0, 0)
         jdays = utc2jul.utc2jul(time) #- utc2jul.utc2jul(epoch_date)
@@ -61,7 +60,6 @@ class magFieldDriver:
         lon_deg = -1.348386750055788e+02
         alt_meter = 3.968562753276266e+05
         return self.testFunction(bV, sV, lat_deg, lon_deg, alt_meter, new_date)
->>>>>>> 46a352038c294b6ab2c1ef514576b20a0d34f301
 
 driver = magFieldDriver()
 print(driver.mainTester())
