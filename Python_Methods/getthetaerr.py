d7c3eeabc69536f44f6ba468b74c0501fbc7505e
Ayush Rautwar <arautwar@gmail.com>
2019-01-31 17:52:36 -0500

    Updated access np.matrix in getqref and getthetaerr

--- Python_Methods/getthetaerr.py
+++ Python_Methods/getthetaerr.py
@@ -2,10 +2,11 @@
 # Calculates error theta for each axis based on error quaternion
 # Translated by Bharath Dileepkumar 10/18/2018
 import numpy as np
+
+
 def getthetaerr(q):
-    q = np.matrix(q) # Initializes q into a row vector (2d matrix in Python)
-    thetaerr = 2 * (q[0:3]/q[3]) # Calculates attitude error in each axis by dividing q by scalar component
-    thetaerr = thetaerr.transpose()
-    thetaerr.shape = (1,3)
+    q = np.matrix(q)  # Initializes q into a row vector (2d matrix in Python)
+    thetaerr = 2 * (q[0, 0:3] / q.item(3))  # Calculates attitude error in each axis by dividing q by scalar component
+    thetaerr = thetaerr.getH()
+    thetaerr.shape = (1, 3)
     return thetaerr
-    
