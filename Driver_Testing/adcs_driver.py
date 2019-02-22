a2951015f9ce2cc422f6c09403308c81898dbf2f
Jason <jay.chen135@gmail.com>
2019-02-22 15:23:46 -0500

    You know the drill @Akash

--- Driver_Testing/adcs_driver.py
+++ Driver_Testing/adcs_driver.py
@@ -34,13 +34,16 @@ def load_config(config_file):
 
 def write_config(config_file, data):
     with open(config_file, 'w') as stream:
-        yaml.dump(data, stream, default_flow_style=False)
+        try:
+            yaml.dump(data, stream, default_flow_style=False)
+        except yaml.YAMLError as error:
+            print(error)
 
 def gps_is_on():
     return True
 
 def tle_get_data():
-    return None;
+    return {};
 
 def main():
     global epoch
@@ -48,20 +51,28 @@ def main():
     # Convert Cartesian coordinates and time to a Keplerian Elements array.
     # Update the config_adcs.yaml file with new KOE array.
     if(gps_is_on()):
-        data = gps_dummy.get_data()
-        r = [data['x_pos'], data['y_pos'], data['z_pos']]
-        vel = [data['x_vel'], data['y_vel'], data['z_vel']]
-        koe_array = cart2kep(r, vel)
-        epoch = data['time']
+        data = get_data() # Data is a list (cache) of dictionaries representing a timestep.
+        i = len(data) # Get the last dictionary in the cache.
+        r = [data[i]['x_pos'], data[i]['y_pos'], data[i]['z_pos']] # Position state vector.
+        vel = [data[i]['x_vel'], data[i]['y_vel'], data[i]['z_vel']] # Velocity state vector.
+        epoch = data[i]['time'] # Datetime object representing the epoch.
+            
+        drag = 2.2 # Fixed drag coefficient.
+        koe_array = cart2kep(r, vel) # Convert state vectors into an array representing the KOE.
+        koe_array = np.append(koe_array, drag) # 
+        koe_array = np.insert(koe_array, 0, utc2jul(epoch))
+        print(koe_array)
+    # If GPS is off, write data to the YAML from the previous TLE file.
+    # 
     else:
-        # write_config('config_adcs.yaml', tle_get_data())
+        write_config('config_adcs.yaml', tle_get_data())
         config = load_config('config_adcs.yaml')
         epoch = datetime.utcnow()
         koe_array = np.array([])
         for key, val in config['adcs']['koe'].items():
             koe_array = np.append(koe_array, val)
-    print(koe_array)
-    config['adcs']['sc']['jd0'] = utc2jul(epoch) # Use write config.
+    # print(koe_array)
+    # config['adcs']['sc']['jd0'] = utc2jul(epoch) # Use write config.
 
 if __name__ == "__main__":
     t1 = threading.Thread(target=main, args=(), daemon=True)
--- Driver_Testing/gps_dummy.py
+++ Driver_Testing/gps_dummy.py
@@ -1,5 +1,5 @@
 from datetime import datetime
 
 def get_data():
-    return [{'lat': 5.3357}, {'lon': -1.3483e+02}, {'alt': 3.9685e+05*3.2808}, {'time': datetime.utcnow()}, {'x_pos': 3000000.}, \
-        {'y_pos': 5000000}, {'z_pos': 1000000}, {'x_vel: 3000'}, {'y_vel: 3000'}, {'z_vel: 5000'}]
+    return [{'lat': 5.3357, 'lon': -1.3483e+02, 'alt': 3.9685e+05*3.2808, 'time': datetime.utcnow(), 'x_pos': 3000000., \
+        'y_pos': 5000000, 'z_pos': 1000000, 'x_vel': 3000, 'y_vel': 3000, 'z_vel': 5000}]
