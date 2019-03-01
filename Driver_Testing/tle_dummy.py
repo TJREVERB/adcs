9dfcfaec3bf84437169e2dc07fa65a443e2eb077
Ayush Rautwar <arautwar@gmail.com>
2019-03-01 15:35:59 -0500

    Add tle_dummy

---
+++ Driver_Testing/tle_dummy.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+import numpy as np
+from pyorbital import tlefile
+from pyorbital.orbital import Orbital
+from pyorbital.orbital import OrbitElements
+from datetime import datetime
+
+def getlla(t):
+    orb = Orbital("TJREVERB", tle_file="tjreverbtle.txt")
+    return({'lat':orb.get_lonlatalt(t)[0],'lon':orb.get_lonlatalt(t)[1],'alt':orb.get_lonlatalt(t)[2]})
