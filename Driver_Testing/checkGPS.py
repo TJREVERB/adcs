334465abf5c67101278f69798fead06a7b9f8266
unknown <bharathdileepkumar@gmail.com>
2019-03-21 16:22:36 -0400

    add to driver

--- Driver_Testing/checkGPS.py
+++ Driver_Testing/checkGPS.py
@@ -1,16 +1,11 @@
 # Written by Bharath Dileepkumar
-# March 17, 2018
+# March 21, 2018
 
 import tle_dummy
 
 def checkGPS(lat, long, alt, time):
     tle = tle_dummy.get_lla(time)
-    if(abs(lat - tle["lat"]) > 2 or abs(long - tle["lon"]) > 2):
+    if(abs(lat - tle["lat"]) > 2 or abs(long - tle["lon"]) > 2 or abs(alt - tle["alt"]) > 2):
         return False
     else:
         return True
-
-
-
-
-
