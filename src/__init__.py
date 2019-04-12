cc1a798b0e08e1b3e7a6b00bdcc3b5c1a4caebd4
ayusher <42046470+ayusher@users.noreply.github.com>
2019-04-11 21:56:47 -0400

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from .get_q_ref import *
+from .get_q_ref_nadir import get_q_ref_nadir
 from .get_dcm import get_dcm
 from .get_theta_err import get_theta_err
 from .get_q_err import get_q_err
